41d23df420a525797759b1b4b03049173210911d
mysgantengs <iyangsahmad@gmail.com>
2024-01-16 12:49:31 +0700

    kita ngomit lagi selasa siang ini

--- app/Http/Controllers/LoginController.php
+++ app/Http/Controllers/LoginController.php
@@ -3,8 +3,21 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use Illuminate\Http\Response;
 
 class LoginController extends Controller
 {
-    //
+    public function Login():Response{
+
+        return response()->view("Users.Login");
+
+    }
+
+    public function ActionLogin(){
+
+    }
+
+    public function Logout(){
+        
+    }
 }
--- routes/web.php
+++ routes/web.php
@@ -16,3 +16,8 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::controller(\App\Http\Controllers\LoginController::class)->group(function () {
+    Route::get('/login', 'Login');
+    Route::post('/login', 'ActionLogin');
+});
